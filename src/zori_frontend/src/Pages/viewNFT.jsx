a29f9796a69962184d3d810691962626c611d41f
k4p1l <k4p1ll.23@gmail.com>
2024-10-29 13:07:13 +0000

    changed css of viewNFT

--- src/zori_frontend/src/Pages/viewNFT.jsx
+++ src/zori_frontend/src/Pages/viewNFT.jsx
@@ -17,43 +17,45 @@ const ViewNFT = ({ categories }) => {
 
   return (
     <div className="nft-details-page">
-      <div className="nft-details">
-        <img src={nft.imageUrl} alt={nft.title} className="nft-image" />
-      </div>
-      <div className="nft-info">
-        <h2>{nft.title}</h2>
-        <p>Description: Add any extra details here if available</p>
-        <p>
-          Price:{" "}
-          <span>
-            {nft.price} {nft.currency}
-          </span>
-        </p>
-        <section class="grid place-content-center py-6">
-          <button
-            class="group flex h-10 items-center gap-2 rounded-full bg-[#c753bd] pl-3 pr-4 transition-all duration-300 ease-in-out hover:bg-black hover:pl-2 hover:text-white active:bg-neutral-700"
-            onClick={handlePurchase}
-          >
-            <span class="rounded-full bg-[#480442] p-1 text-sm transition-colors duration-300 group-hover:bg-white">
-              <svg
-                stroke="currentColor"
-                fill="none"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="-translate-x-[200%] text-[0px] transition-all duration-300 group-hover:translate-x-0 group-hover:text-lg group-hover:text-black group-active:-rotate-45"
-                height="1em"
-                width="1em"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <line x1="5" y1="12" x2="19" y2="12"></line>
-                <polyline points="12 5 19 12 12 19"></polyline>
-              </svg>
+      <div className="nft-details-wrapper">
+        <div className="nft-details">
+          <img src={nft.imageUrl} alt={nft.title} className="nft-image" />
+        </div>
+        <div className="nft-info">
+          <h2>{nft.title}</h2>
+          <p>Description: Add any extra details here if available</p>
+          <p>
+            Price:{" "}
+            <span>
+              {nft.price} {nft.currency}
             </span>
-            <span>Buy Now</span>
-          </button>
-        </section>
+          </p>
+          <section class="grid place-content-center py-6">
+            <button
+              class="group flex h-10 items-center gap-2 rounded-full bg-[#c753bd] pl-3 pr-4 transition-all duration-300 ease-in-out hover:bg-black hover:pl-2 hover:text-white active:bg-neutral-700"
+              onClick={handlePurchase}
+            >
+              <span class="rounded-full bg-[#480442] p-1 text-sm transition-colors duration-300 group-hover:bg-white">
+                <svg
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="-translate-x-[200%] text-[0px] transition-all duration-300 group-hover:translate-x-0 group-hover:text-lg group-hover:text-black group-active:-rotate-45"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <line x1="5" y1="12" x2="19" y2="12"></line>
+                  <polyline points="12 5 19 12 12 19"></polyline>
+                </svg>
+              </span>
+              <span>Buy Now</span>
+            </button>
+          </section>
+        </div>
       </div>
     </div>
   );
