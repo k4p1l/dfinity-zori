be564bde87d39a2f285e1afadcee1ae62e742bac
k4p1l <k4p1ll.23@gmail.com>
2024-11-04 11:17:42 +0000

    added explore buttons on features section

--- src/zori_frontend/src/Features.jsx
+++ src/zori_frontend/src/Features.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import zori_land from "../public/images/spaces/zori land.png";
 import zori_avatar from "../public/images/nfts/4.jpg";
 import nft_avatar from "../public/images/nfts/tr03.png";
@@ -19,16 +20,16 @@ const Features = () => {
                 <span>3</span>D Spaces
               </h1>
               <p>
-                Immersive 3D Space in the Metaverse Enter a Web3-powered
-                metaverse where every action is a game, and every player is a
-                creator! Experience themed zones, collaborative missions, and
-                real-time interactions that empower users to play and earn
-                within an evolving digital world. The system is based on
-                high-quality 3D space, running on the Unity engine's WebGL, and
-                supports various mobile devices, compatible with mainstream
-                browsers, ensuring users can enjoy immersive experiences in the
-                metaverse in various environments
+                Step into a Web3-powered metaverse where play meets creation!
+                Engage in themed zones, collaborative missions, and real-time
+                interactions—all within a high-quality 3D space, built on
+                Unity's WebGL for cross-device accessibility.
               </p>
+              <Link to="/spaces">
+                <button class="overflow-hidden cursor-pointer text-white rounded px-4 py-2 bg-[radial-gradient(100%_100%_at_100%_0%,_#af8bee_0%,_#783bcd33_100%)] transition-[box-shadow_0.15s_ease,_transform_0.15s_ease] shadow-md hover:shadow-lg hover:-translate-y-0.5 active:translate-y-0.5 active:shadow-[inset_0px_3px_7px_#783bcd33] ">
+                  Explore 3D Spaces
+                </button>
+              </Link>
             </div>
           </div>
           <div className="f-row2">
@@ -38,15 +39,16 @@ const Features = () => {
             <div className="f-content">
               <h1>NFT Marketplace</h1>
               <p>
-                Robust NFT Management and Marketplace NFT management introduces
-                a powerful digital asset layer to The zori, allowing users to
-                tokenize avatars, virtual land plots, digital artworks, and more
-                as unique NFTs. This system empowers users to own, trade, and
-                showcase their assets, bringing real value, scarcity, and
-                collectability to The zori virtual economy. By adding this layer
-                of ownership, users can truly invest in and shape their
-                metaverse experience.
+                Own, trade, and showcase digital assets—from avatars to land
+                plots—with our NFT management system. Empowering users with real
+                value and collectability, it lets you shape your
+                metaverse experience.
               </p>
+              <Link to="/marketplace">
+                <button class="overflow-hidden cursor-pointer text-white rounded px-4 py-2 bg-[radial-gradient(100%_100%_at_100%_0%,_#af8bee_0%,_#783bcd33_100%)] transition-[box-shadow_0.15s_ease,_transform_0.15s_ease] shadow-md hover:shadow-lg hover:-translate-y-0.5 active:translate-y-0.5 active:shadow-[inset_0px_3px_7px_#783bcd33] ">
+                  Explore NFT Marketplace
+                </button>
+              </Link>
             </div>
           </div>
         </div>
@@ -57,18 +59,16 @@ const Features = () => {
           <div className="f-content">
             <h1>AI Avatar</h1>
             <p>
-              Personalized AI Avatars and Zori Avatars <br /> Explore two
-              exciting avatar options in our gamified Web3 metaverse: <br /> The
-              AI Avatar and the Zori Avatar. With the AI Avatar, users can
-              instantly create a personalized digital identity just by clicking
-              an image, bringing their likeness to life in seconds. Meanwhile,
-              the Zori Avatar offers rich customization, allowing users to craft
-              a unique look and enhance it further with NFT wearables that can
-              be purchased and worn, adding exclusive style elements to your
-              digital self. Both avatars empower users to interact, create, and
-              play in immersive ways, bringing out the best of each personality
-              within the metaverse!
+              Bring your digital identity to life with the AI Avatar for instant
+              likeness or customize your Zori Avatar with NFT wearables. Both
+              avatars offer unique, immersive ways to interact and express
+              yourself in the metaverse.
             </p>
+            <Link to="/avatar">
+              <button class="overflow-hidden cursor-pointer text-white rounded px-4 py-2 bg-[radial-gradient(100%_100%_at_100%_0%,_#af8bee_0%,_#783bcd33_100%)] transition-[box-shadow_0.15s_ease,_transform_0.15s_ease] shadow-md hover:shadow-lg hover:-translate-y-0.5 active:translate-y-0.5 active:shadow-[inset_0px_3px_7px_#783bcd33] ">
+                Explore 3D Avatar{" "}
+              </button>
+            </Link>
           </div>
         </div>
       </div>
